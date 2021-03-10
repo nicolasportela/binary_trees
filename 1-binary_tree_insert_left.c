ef41f4c3a49eac52027dfbd28f22b7c465b13837
nicolasportela <2103@holbertonschool.com>
2021-03-10 02:41:08 +0000

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,5 +9,24 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_left;
 
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+	new_left = malloc(sizeof(binary_tree_t));
+	if (new_left == NULL)
+	{
+		return (NULL);
+	}
+	new_left->parent = parent;
+	new_left->n = value;
+	if (parent->left != NULL)
+	{
+		new_left->left = parent->left;
+		new_left->left->parent = new_left;
+	}
+	parent->left = new_left;
+	return (new_left);
 }
