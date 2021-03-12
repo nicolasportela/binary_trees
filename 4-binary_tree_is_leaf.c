0c2b847b5e224a9eba04ccac32e0dbf65272dc97
nicolasportela <2103@holbertonschool.com>
2021-03-12 01:24:15 +0000

    task 4 edited and task 5

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - function that deletes an entire binary tree
+ * binary_tree_is_leaf - function that checks if a node is a leaf
  * @node: pointer to the node to check
  * Return: 1 if node is a leaf, otherwise or NULL 0
  */
